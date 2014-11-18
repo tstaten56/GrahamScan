6bbb08fdbc3834eb63f911b7ec128897598c2e19
tstaten56 <tstaten56@gmail.com>
2014-11-18 15:21:55 -0500

    Adding the beginnings of GrahamScan.h

--- GrahamScan.h
+++ GrahamScan.h
@@ -1,1 +1,15 @@
-.
+#pragma once
+#ifndef GRAHAMSCAN_H
+#include <vector>
+using namespace std;
+class GrahamScan
+{
+private:
+
+public:
+	GrahamScan();
+	void readFile(string filename);
+	void outputFile();
+	
+};
+#endif
--- main.cpp
+++ main.cpp
@@ -1,1 +1,22 @@
-.
+#include <iostream>
+#include "GrahamScan.h"
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	if (argc == 2)
+	{
+		string file;
+		int vert, horz;
+		file = argv[1];
+		GrahamScan ex;
+		ex.readFile(file);
+	}
+	else
+	{
+		cout << "Wrong input format!" << endl;
+		return 1;
+	}
+
+	return 0;
+}
