34da7c50b397f71b02c41187748559db73e83614
tstaten56 <tstaten56@gmail.com>
2014-11-21 13:56:26 -0500

    Added adjustCoords, getAngle, calcAngle
    May need more things, not sure

--- Node.cpp
+++ Node.cpp
@@ -1,1 +1,52 @@
-.
+#include "Node.h"
+
+Node::Node()
+{
+}
+
+Node::Node(double xVal, double yVal)
+{
+	x = xVal;
+	y = yVal;
+}
+
+Node::~Node()
+{
+}
+
+double Node::getX()
+{
+	return x;
+}
+
+double Node::getY()
+{
+	return y;
+}
+
+double Node::getAngle()
+{
+	return angle;
+}
+
+void Node::calcAngle()
+{
+	angle = y / x; //Not sure if this is right yet, it was in my notes like this though
+}
+
+void Node::setX(double xVal)
+{
+	x = xVal;
+}
+
+void Node::setY(double yVal)
+{
+	y = yVal;
+}
+
+void Node::adjustCoords(double previousX, double previousY)
+{
+	//The node for 0,0 has been chosen, now we have to adjust all the other nodes to align with its previous coords
+	x = x - previousX;
+	y = y - previousY;
+}
