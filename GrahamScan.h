411a7533bcb75b96ed2cdd26261e26edc6ba26d3
tstaten56 <tstaten56@gmail.com>
2014-11-21 13:55:21 -0500

    Added some more functions
    Most these things aren't done, just readFile() is complete

--- GrahamScan.h
+++ GrahamScan.h
@@ -1,15 +1,26 @@
 #pragma once
 #ifndef GRAHAMSCAN_H
 #include <vector>
+#include "Node.h"
+
 using namespace std;
 class GrahamScan
 {
 private:
-
+	//An array of Nodes, Node* arr; or  vector<Node> arr; might be better! 
+	vector<Node> stack; //For hull-making
+	Node* graph; //Holds the entire graph
+	int numOfNodes;
+	
 public:
 	GrahamScan();
-	void readFile(string filename);
-	void outputFile();
-	
+	~GrahamScan();
+	void readFile(string filename); //reads the file and calls required execution
+	void outputFile(string filename); //outputs file named   original_hull.txt
+	void turnDirection(int position1, int position2); //Calculates which turn we are taking
+	void setNumOfNodes(int num);
+	int getNumOfNodes();
+	void scan(); //This is the main event
+	void findLowestY();
 };
 #endif
